f7829cf10f74025376ea4e8a93cab409a1d99919
Tom Hanson <thanson@and.digital>
2022-01-28 21:33:13 +0000

    :tada: first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import BottomTabNavigator from "./src/navigation/TabNavigator";
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <BottomTabNavigator />
+    </NavigationContainer>
+  );
+}
+export default App
---
+++ src/navigation/TabNavigator.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { MainStackNavigator, ContactStackNavigator } from "./StackNavigator";
+
+const Tab = createBottomTabNavigator();
+
+const screenOptionStyle = {
+  headerShown: false
+};
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator screenOptions={screenOptionStyle}>
+      <Tab.Screen name="Home" component={MainStackNavigator} />
+      <Tab.Screen name="Contact" component={ContactStackNavigator} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
